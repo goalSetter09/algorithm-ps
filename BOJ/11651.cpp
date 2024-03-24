aab006c2fbcd32c9ce33dc5b0e1598a07ec36398
dongyoung <dong0579@naver.com>
2024-03-24 14:05:47 +0900

    Refactor: 폴더 위치 BOJ로 변경

---
+++ BOJ/11651.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define X first
+#define Y second
+using namespace std;
+
+vector<pair<int, int> > point;
+
+bool cmp(const pair<int, int> &a, const pair<int, int> &b) {
+    if(a.Y < b.Y) {
+        return true;
+    } else if(a.Y > b.Y) {
+        return false;
+    }else if(a.X <= b.X) {
+        return true;
+    } else return false;
+}
+
+void print(const pair<int, int> &p) {
+    cout << p.X << ' ' << p.Y << '\n';
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    int x, y;
+    for(int i = 0; i < n; i++) {
+        cin >> x >> y;
+        point.push_back(make_pair(x, y));
+    }
+
+    sort(point.begin(), point.end(), cmp);
+
+    for_each(point.begin(), point.end(), print);
+}
---
+++ BOJ/2004.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long d[2000000005][2];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n, m;
+    cin >> n >> m;
+
+    /*
+     * n , m = n! * m !
+     *          n -m !
+     *
+     *          5 2 = 5 4 / 321 = 5 * 4 * 3 * 2 * 1
+     *                                3 * 2 * 1
+     */n! * n-m ! / m!
+
+}
